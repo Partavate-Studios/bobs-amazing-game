04ae9ed60428fb7f90dcb578c371adcf8f6da091
Koda <zaskoda@gmail.com>
2023-09-18 16:22:34 -0600

    Increment button lives in Vue

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,8 +1,54 @@
-<script setup lang="ts">
-console.log('test')
+<script lang="ts">
+import { setup } from "./mud/setup"
+import mudConfig from "contracts/mud.config";
+
+const {
+  components,
+  systemCalls: { increment },
+  network,
+} = await setup();
+
+// https://vitejs.dev/guide/env-and-mode.html
+/*
+if (import.meta.env.DEV) {
+  const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+  mountDevTools({
+    config: mudConfig,
+    publicClient: network.publicClient,
+    walletClient: network.walletClient,
+    latestBlock$: network.latestBlock$,
+    blockStorageOperations$: network.blockStorageOperations$,
+    worldAddress: network.worldContract.address,
+    worldAbi: network.worldContract.abi,
+    write$: network.write$,
+    recsWorld: network.world,
+  });
+}
+*/
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    async update() {
+        console.log("new counter value:", await increment());
+    }
+  },
+  mounted() {     
+    components.Counter.update$.subscribe((update) => {
+        const [nextValue, prevValue] = update.value;
+        this.counter = nextValue?.value ?? 0
+    });
+  }    
+}
+
 </script>
 
 
 <template>
-    <p>Vue Works</p>
+    <p>Vue Counter</p>
+    <button @click="update()">Button</button>
+    {{ counter }}
 </template>
