6151d69492e9edff203820de5789771f5f5a3faf
Koda <zaskoda@gmail.com>
2025-02-08 15:20:59 -0600

    svg scaffolding

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,85 +1,53 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import svgContainer from './containers/svg.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <svgContainer>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
 
-  <RouterView />
+  </svgContainer>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss">
+body {
+  font-family: 'Ubuntu', sans-serif;
+  background: var(--color-bg);
+  color: var(--color-light);
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+  padding: 0;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+#app {
   text-align: center;
-  margin-top: 2rem;
+}
+.clickable:hover {
+  cursor: pointer;
+}
+</style>
+
+
+<style lang="scss">
+//Default Theme
+:root {
+  --color-bg: #2222ff;
+  --color-text: #ffffff;
+  --color-btn-text: #000000;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+//Orbiter 8 Theme
+[data-theme="orbiter8"] {
+  --color-bg: #000000;
+  --color-light: #c3b3da;
+  --color-btn: #ffffff;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+//Test Theme
+[data-theme="evil"] {
+  --color-bg: #440000;
 }
 </style>
